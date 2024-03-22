ce67aecac32ce336a6a2245a18f26937cf47dd0d
Gabriel Lira <107827829+GabrieelLira@users.noreply.github.com>
2024-03-22 01:52:41 -0300

    Update product_serializer.py

--- product/serializers/product_serializer.py
+++ product/serializers/product_serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from product.models.product import Product
 from product.serializers.category_serializer import CategorySerializer
 
-class ProdcutSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(required=True, many=True)
 
     class Meta:
